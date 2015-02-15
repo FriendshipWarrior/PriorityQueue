c331c07e20e9c171470a5d093addf8532e60860b
Dustin Hurst <the_friendship_warrior@hotmail.com>
2015-02-14 20:11:31 -0700

    Initial Commit

---
+++ CS 2450 PriorityQueue/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS 2450 PriorityQueue
+//
+//  Created by Dustin Hurst on 2/14/15.
+//  Copyright (c) 2015 Dustin Hurst. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
